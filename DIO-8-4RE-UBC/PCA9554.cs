41e40dd5531b2dd68e172318fbaa8a32b1636c64
Haruyoshi Iguchi <h-iguchi@users.noreply.github.com>
2017-07-21 16:27:10 +0900

    リファクタリング。ただし、他の言語にも移植しやすいように、C#特有の機能（var, null演算子など）は極力使用していない。

--- DIO-8-4RE-UBC/PCA9554.cs
+++ DIO-8-4RE-UBC/PCA9554.cs
@@ -1,90 +1,108 @@
-﻿// 
-// Copyright (c) 2017 Y2 Corporation
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Pca9554.cs" company="Y2 Corporation">
+//   Copyright (c) 2017 Y2 Corporation. All rights reserved.
+// </copyright>
+// <license>
+//   MIT License
 // 
-// All rights reserved. 
-// 
-// MIT License
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy of
-// this software and associated documentation files (the "Software"), to deal in
-// the Software without restriction, including without limitation the rights to
-// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-// the Software, and to permit persons to whom the Software is furnished to do so,
-// subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-// 
+//   Permission is hereby granted, free of charge, to any person obtaining a copy of
+//   this software and associated documentation files (the "Software"), to deal in
+//   the Software without restriction, including without limitation the rights to
+//   use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//   the Software, and to permit persons to whom the Software is furnished to do so,
+//   subject to the following conditions:
+//   
+//   The above copyright notice and this permission notice shall be included in all
+//   copies or substantial portions of the Software.
+//   
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//   FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//   COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//   IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </license>
+// --------------------------------------------------------------------------------------------------------------------
 
-namespace Y2C_I2C_Libraries {
-    class PCA9554 {
+namespace DIO_8_4RE_UBC
+{
+    public class Pca9554
+    {
+        private readonly I2CFt4222 i2C;
+        private readonly byte slaveAddress;
+        private byte outputValue = 0xff;
+        private byte portDirection = 0xff;
 
-        private const byte PortMax = 1;
+        public Pca9554(I2CFt4222 i2C, byte slaveAddress)
+        {
+            this.i2C = i2C;
+            this.slaveAddress = slaveAddress;
+        }
 
-        public enum Register : byte {
+        public enum Register : byte
+        {
             InputPort,
             OutputPort,
             PolarityInversion,
             IoConfiguration
         }
 
-        public enum PinDirection {
+        public enum PinDirection
+        {
             Output,
             Input
         }
 
-        private byte slaveAddress;
-        private byte outputValue = 0xff;
-        private byte portDirection = 0xff;
+        public ResultCode SetPortDirection(byte value)
+        {
+            byte[] writeBuffer = { (byte)Register.IoConfiguration, value };
+            if (i2C == null)
+                return ResultCode.FatalError;
 
-        private I2C_FT4222 i2c;
+            ResultCode result = i2C.Write(slaveAddress, ref writeBuffer[0], (ushort)writeBuffer.Length);
+            if (result != ResultCode.Ok)
+                return result;
 
-        public PCA9554(I2C_FT4222 i2c, byte slaveAddress) {
-            this.i2c = i2c;
-            this.slaveAddress = slaveAddress;
+            portDirection = value;
+            return ResultCode.Ok;
         }
 
-        public ResultCode SetPortDirection(byte value) {
-            byte[] writeBuffer = new byte[] { (byte)Register.IoConfiguration, value };
-            ResultCode result = i2c.Write(slaveAddress, ref writeBuffer[0], (ushort)writeBuffer.Length);
-            return ResultCode.OK;
-        }
-
-        public ResultCode SetPinDirection(int pin, PinDirection pinDir) {
-            if ((pin < 0) || (7 < pin))
+        public ResultCode SetPinDirection(int pin, PinDirection pinDir)
+        {
+            if (pin < 0 || 7 < pin)
                 return ResultCode.InvalidParameter;
+
             byte value;
             if (pinDir == PinDirection.Input)
                 value = (byte)(portDirection | (1 << pin));
             else
                 value = (byte)(portDirection & ~(1 << pin));
-            return SetPortDirection(portDirection);
+            return SetPortDirection(value);
         }
 
-        public ResultCode ReadRegister(Register register, out byte value) {
+        public ResultCode ReadRegister(Register register, out byte value)
+        {
             value = 0;
-            byte[] writeBuffer = new byte[] { (byte)register };
-            ResultCode result = i2c.WriteEx(slaveAddress, (byte)LibFT4222.I2cMasterFlag.Start, ref writeBuffer[0], (ushort)writeBuffer.Length);
-            if (result != ResultCode.OK)
-                return result;
-            return i2c.ReadEx(slaveAddress, (byte)(LibFT4222.I2cMasterFlag.RepeatedStart | LibFT4222.I2cMasterFlag.Stop), ref value, 1);
+            byte[] writeBuffer = { (byte)register };
+            if (i2C == null)
+                return ResultCode.FatalError;
 
+            ResultCode result = i2C.WriteEx(slaveAddress, (byte)LibFt4222.I2CMasterFlag.Start, ref writeBuffer[0], (ushort)writeBuffer.Length);
+            if (result != ResultCode.Ok)
+                return result;
+
+            return i2C.ReadEx(slaveAddress, (byte)(LibFt4222.I2CMasterFlag.RepeatedStart | LibFt4222.I2CMasterFlag.Stop), ref value, 1);
         }
 
-        public ResultCode ReadPort(out byte value) {
+        public ResultCode ReadPort(out byte value)
+        {
             return ReadRegister(Register.InputPort, out value);
         }
 
-        public ResultCode ReadPin(int pin, out bool state) {
+        public ResultCode ReadPin(int pin, out bool state)
+        {
             state = true;
-            if ((pin < 0) || (7 < pin))
+            if (pin < 0 || 7 < pin)
                 return ResultCode.InvalidParameter;
             byte ip;
             ResultCode result = ReadPort(out ip);
@@ -92,16 +110,22 @@ namespace Y2C_I2C_Libraries {
             return result;
         }
 
-        public ResultCode WritePort(byte value) {
-            byte[] writeBuffer = new byte[] { (byte)Register.OutputPort, value };
-            ResultCode result = i2c.Write(slaveAddress, ref writeBuffer[0], (ushort)writeBuffer.Length);
-            if (result != ResultCode.OK)
+        public ResultCode WritePort(byte value)
+        {
+            byte[] writeBuffer = { (byte)Register.OutputPort, value };
+            if (i2C == null)
+                return ResultCode.FatalError;
+
+            ResultCode result = i2C.Write(slaveAddress, ref writeBuffer[0], (ushort)writeBuffer.Length);
+            if (result != ResultCode.Ok)
                 return result;
-            return ResultCode.OK;
+            outputValue = value;
+            return ResultCode.Ok;
         }
 
-        public ResultCode WritePin(int pin, bool state) {
-            if ((pin < 0) || (7 < pin))
+        public ResultCode WritePin(int pin, bool state)
+        {
+            if (pin < 0 || 7 < pin)
                 return ResultCode.InvalidParameter;
             byte value;
             if (state)
@@ -110,6 +134,5 @@ namespace Y2C_I2C_Libraries {
                 value = (byte)(outputValue & ~(1 << pin));
             return WritePort(value);
         }
-
     }
 }
--- DIO-8-4RE-UBC/Program.cs
+++ DIO-8-4RE-UBC/Program.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Y2C Corporation">
+//   Y2 Corporation
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
 using System.Windows.Forms;
 
-namespace DIO_8_4RE_UBC {
-    static class Program {
+namespace DIO_8_4RE_UBC
+{
+    internal static class Program
+    {
         /// <summary>
         /// アプリケーションのメイン エントリ ポイントです。
         /// </summary>
         [STAThread]
-        static void Main() {
+        private static void Main()
+        {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
